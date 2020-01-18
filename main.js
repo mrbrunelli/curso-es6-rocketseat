f87259f20aceedf639852c16a785b29ae16058fc
Matheus Ricardo Brunelli <matheus.brunelli@gazin.com.br>
2020-01-18 12:08:41 -0300

    entendendo classes

--- main.js
+++ main.js
@@ -1,11 +1,31 @@
-alert('teste')
-
-class Teste {
-    metodo() {
-
+class List {
+    constructor() {
+        this.data = []
     }
 
-    outro() {
-
+    add(data) {
+        this.data.push(data)
+        console.log(this.data)
     }
 }
+
+
+class TodoList extends List {
+    constructor() {
+        super()
+
+        this.usuario = 'Matheus'
+    }
+
+    mostraUsuario() {
+        console.log(this.usuario)
+    }
+}
+
+var MinhaLista = new TodoList()
+
+document.getElementById('novotodo').onclick = function() {
+    MinhaLista.add('Novo todo')
+}
+
+MinhaLista.mostraUsuario()
